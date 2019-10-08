faf747ebeb5743cc4ab9aac97e5af6d9fe66aa09
Zachary Sanchez <Zacharyesanchez22@gmail.com>
2019-10-08 14:01:09 -0600

    started phase 1 for data design project

---
+++ epic/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8"/>
+		<title>Data Design Project</title>
+		<link rel="stylesheet" href="style.css">
+	</head>
+	<body>
+	<h1>Zachary's Data Design Project-Index</h1>
+	<a href=phase-1.php>Phase 1</a>
+	</body>
+</html>
---
+++ epic/phase-1.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8"/>
+	<title>Phase 1</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<h1 class="pg1">Persona</h1>
+
+<p>John is a 32yo food critic and writter. He has recently moved to Albuquerque, NM and has been trying all the
+different foods the city has to offer. He has had some good and some bad experiences and has been adding up
+his data and information to post onto different sites for others to view. He has recently been working on Yelp
+to post his food reviews. He needs to be able to access the site and quickly enter a review due to always being
+in a rush to get to the next food spot.</p>
+
+<img src="yelp.png" alt="yelp" class="center">
+
+<h2>User Story</h2>
+
+<p>As an elite member user, John wants to post a food review.</p>
+
+<h2>Use Case/Interaction</h2>
+
+<p>As a user of this site John will be able to login as a member and search for food in the area or a specific
+restaurant to view other reviews and post one for himself. To do this he would follow the steps below.</p>
+
+<div class="center">
+<ul class="center-list">
+	<li>Go to Yelp.com</li>
+	<li>On homepage, select login</li>
+	<li>Enter login information and select login again</li>
+	<li>Login will verify on the server</li>
+	<li>Click on the search bar</li>
+	<li>Enter name a of restaurant to find</li>
+	<li>Select restaurant</li>
+	<li>When on restaurant page, select write a review</li>
+	<li>Select amount of stars you wish to give</li>
+	<li>Enter text into the review area</li>
+	<li>Select upload photos and choose photos</li>
+	<li>Select Post Review</li>
+</ul>
+</div>
+
+</body>
+</html>

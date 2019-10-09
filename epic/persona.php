9169a1e37bc4f767e1f2e7ce516ef09cc87ea246
Zachary Sanchez <Zacharyesanchez22@gmail.com>
2019-10-09 15:39:36 -0600

    added an additional photo

--- epic/persona.php
+++ epic/persona.php
@@ -9,6 +9,8 @@
 
 <h1 class="pg1">Persona</h1>
 
+<img src="worl.jpg" alt ="worl" class="center">
+
 <p>John is a 32yo food critic and writter. He has recently moved to Albuquerque, NM and has been trying all the
 different foods the city has to offer. He has had some good and some bad experiences and has been adding up
 his data and information to post onto different sites for others to view. He has recently been working on Yelp
